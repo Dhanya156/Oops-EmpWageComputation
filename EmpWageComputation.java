0d1f0b5f87a6ba3266ffb3fe9c41946632c99cb0
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-16 04:29:02 -0700

    Added:Wage For Total working Days And Hour

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,12 +3,14 @@ public class EmpWageComputation {
 	        public static final int WAGE_PER_HR=20;
 	        public static final int FULL_TIME=1;
 	        public static final int PART_TIME=2;
-	        public static final int NUMBER_OF_WORKING_DAYS=20;
+		public static final int MAX_WORKING_DAYS=20;
+		public static final int MAX_WORKING_HRS=100;
 	        public static void main(String[] args)
 	        {
-	                int salary,workingHr=0;
-	                int totalSalary=0;
-	                for(int day=0;day<NUMBER_OF_WORKING_DAYS ;day++) {
+	                int salary,totalWorkingHr=0,totalWorkingDays=0;
+	                int totalSalary=0,workingHr;
+	                while(totalWorkingHr < MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS){
+			totalWorkingDays++;
 	                int random=(int)Math.floor(Math.random()*10)%3;
 	                System.out.println(random);
 	                switch(random)
@@ -25,11 +27,11 @@ public class EmpWageComputation {
 	                        System.out.println("Absent");
 	                        workingHr=0;
 	                }
-	                salary=workingHr*WAGE_PER_HR;
-	                totalSalary+=salary;
-	                System.out.println("Wage: "+salary);
+			totalWorkingHr=totalWorkingHr+workingHr;
 	                }
-	                System.out.println(totalSalary);
+			totalSalary=totalWorkingHr*WAGE_PER_HR;
+			System.out.println("totalWorkingHr" +totalWorkingHr);
+	                System.out.println("totalSalary" +totalSalary);
 	        }
 
 }
