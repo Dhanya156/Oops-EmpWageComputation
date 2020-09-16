bb79a9f13917db857d005385ec6a7cf0b45b38ae
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-16 02:49:36 -0700

    Added:Employee Present Or Absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,12 @@
-class EmpWageComputation
+public class EmpWageComputation
 {
-	public static void main(String[] args)
-	{
-	System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION");
-	}
+        public static void main(String[] args)
+        {
+                int IS_PRESENT=1;
+                double empCheck=Math.floor(Math.random()*10)%2;
+                if (empCheck==IS_PRESENT)
+                        System.out.println("Employee is Present");
+                else
+                        System.out.println("Employee is Absent");
+        }
 }
